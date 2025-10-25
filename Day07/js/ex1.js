83c3175c1cbf9ee8eb73b0cee42f6e36310a0211
Nguyen-Thi-Tra-Mi-0304 <traminguyen0304@gmail.com>
2025-10-25 22:41:42 +0700

    Day6_Day7

---
+++ Day07/js/ex1.js
@@ -0,0 +1,13 @@
+var name = "Trà Mi";
+console.log("Welcome to, " , name); 
+
+var age1 = 50;
+console.log("Welcome to, ", name, "Tuổi: ", age1);
+
+let age = 20; 
+var name = "Mi";
+const pi = 3.14159; 
+console.log(age, name, pi);
+
+age = 100; 
+console.log(age, name, pi); 
